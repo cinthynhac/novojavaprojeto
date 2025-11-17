43e4d51ad72c83ace324ecad0eebea6597c79053
cinthynhac <cinthya.allvez@gmail.com>
2025-11-17 20:07:40 -0300

    Atualização 1

---
+++ src/Loja.java
@@ -0,0 +1,9 @@
+public class Loja {
+    private String razaosocial;
+    private String nomefantasia;
+    private String categoriadaloja;
+    private String enderecocomercial;
+
+
+
+}
--- src/User.java
+++ src/User.java
@@ -1,5 +1,45 @@
 public class User {
     protected String nome;
     protected double cpf;
+    protected int telefone;
+    protected String email;
 
+
+
+
+
+
+    //geters and setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(double cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
